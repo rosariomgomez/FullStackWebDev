1f6743f1a3e8392bdf40d7d39c63652a88df4242
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-10 17:58:19 -0800

    media related classes

--- project1/programming_foundations/lesson3/movies/media.py
+++ project1/programming_foundations/lesson3/movies/media.py
@@ -1,16 +1,44 @@
 import webbrowser
 
-class Movie():
-	"""This class provides a way to store movie related information"""
-	
+class Video():
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-	def __init__(self, movie_title, movie_storyline, poster_image, 
-				trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
+	def __init__(self, title, storyline, poster_image_url):
+		self.title = title
+		self.storyline = storyline
+		self.poster_image_url = poster_image_url
+
+class Movie(Video):
+	"""This class provides a way to store movie related information"""
+
+	def __init__(self, movie_title, movie_storyline, movie_duration,
+				poster_image, trailer_youtube):
+		Video.__init__(self, movie_title, movie_storyline, 
+					   poster_image)
 		self.trailer_youtube_url = trailer_youtube
+		self.duration = movie_duration
 
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
+
+class Episode():
+	"""This class provides a way to store information about tvshows' 
+	episodes"""
+	def __init__(self, episode_title, episode_storyline, 
+				episode_duration, episode_season, episode_number):
+		self.title = episode_title
+		self.storyline = episode_storyline
+		self.duration = episode_duration 
+		self.season = episode_season
+		self.number = episode_number
+
+class TvShow(Video):
+	"""This class provides a way to store TvShow related information"""
+
+	def __init__(self, tvshow_title, tvshow_storyline, 
+				poster_image, tv_station, episodes):
+		Video.__init__(self, tvshow_title, tvshow_storyline, 
+					   poster_image)
+		self.tv_station = tv_station
+		self.episodes = episodes
+
